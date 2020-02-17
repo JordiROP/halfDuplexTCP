3b41932a06f1039ad4bf2b4fa2acbbd2fb5d9286
Jord Onrubia Palacios <jordirop@gmail.com>
2020-02-17 19:31:56 +0100

    sender has multiprocessinf now

--- Sender/sender.py
+++ Sender/sender.py
@@ -1,3 +1,8 @@
+from threading import Thread
+from multiprocessing import Process, Manager
+from sympy import sieve
+
+import logging
 import socket
 import sys
 import time
@@ -7,23 +12,49 @@ PORT = 20001
 
 def create_socket():
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    message = str.encode('Hi, im the client')
-    try:
-        for _ in range(5):
-            time.sleep(2)
-            # Send data
-            print("SEND: " + str(message))
-            sent = sock.sendto(message, (IP, PORT))
-            print("SENT: " + str(sent))
+    logging.info("Socket created")
+    return sock
 
-        # Receive response
-        print("Waiting response")
+def start_processes(sock):
+    manager = Manager()
+    shared_queue = manager.Queue()
+    listener_process = Process(target=recieve, args=(shared_queue, sock))
+    response_process = Process(target=send, args=(shared_queue, sock))
+    listener_process.start()
+    response_process.start()
+    keyboard(listener_process, response_process)
+
+def recieve(shared_queue, sock):
+    while True:
         data, adress = sock.recvfrom(4096)
         print("RECV: " + str(data))
         print("RECV: " + str(adress))
+
+def send(shared_queue, sock):
+    message = {'num': None, 'ignore': False}
+    sieve.extend(200)
+    try:
+        for x in range(200):
+            time.sleep(1)
+            if x in sieve:
+                message['ignore'] = True
+            print("SEND: " + str(message))
+            sent = sock.sendto(str.encode(str(message)), (IP, PORT))
+            print("SENT: " + str(sent))
     finally:
         print("CLOSING")
         sock.close()
 
+def keyboard(listener_process, response_process):
+    logging.info('Keyboard listening')
+    while True:
+        inpt = input()
+        if inpt == 'f' or inpt == 'F':
+            listener_process.terminate()
+            response_process.terminate()
+            sys.exit()
+
 if __name__ == "__main__":
-    create_socket()
+    logging.basicConfig(level=logging.INFO)
+    sock = create_socket()
+    start_processes(sock)
